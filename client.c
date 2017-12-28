0666de9acce66d238109f4bfa72c7a7328ae9bd4
LCY <cylin910021@gmail.com>
2017-12-28 18:15:03 +0800

    Last commit is wrong! This commit is correct.

--- client.c
+++ client.c
@@ -5,27 +5,58 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <time.h>
 
 
 int main(int argc , char *argv[])
 {
+    srand(time(NULL));
+    int randNum = rand() % 65534 + 1;
+    int portNum;
+    if(randNum < 1024){
+        randNum += 1024 + (rand() % 10000 + 1);
+    }
+
+    portNum = randNum;
+
+    printf("port number is %d\n", portNum);
+
+    int normalFlag = 0, priorityFlag = 0;  //two connection flag
+
+    fd_set master_set, read_set;
+    int maxfd;
 
     //socket bulid
-    int sockfd = 0;
+    int sockfd = 0, listenfd = 0, prifd = 0;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd == -1){
+    if (sockfd == -1 || listenfd == -1){
         perror("socket()");
     }
 
-    struct sockaddr_in serverInfo;
+    struct sockaddr_in serverInfo, priInfo;
     bzero(&serverInfo, sizeof(serverInfo));
-    serverInfo.sin_family = PF_INET;
+    bzero(&serverInfo, sizeof(priInfo));
 
-    //localhost test
+    serverInfo.sin_family = PF_INET;
+    priInfo.sin_family = PF_INET;
+
+    //localhost
     serverInfo.sin_addr.s_addr = inet_addr("127.0.0.1");
     serverInfo.sin_port = htons(8700);
 
+    priInfo.sin_addr.s_addr = INADDR_ANY;
+    priInfo.sin_port = htons(portNum);
+
+    bind(listenfd,(struct sockaddr *)&priInfo,sizeof(priInfo));
+
+    listen(listenfd, 5);
+    FD_ZERO(&master_set);
+    FD_SET(listenfd, &master_set);
+    FD_SET(sockfd, &master_set);
+
+    maxfd = listenfd; //> listenfd ? sockfd : listenfd;
 
     int err = connect(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
     if(err == -1){
@@ -33,32 +64,118 @@ int main(int argc , char *argv[])
         exit(0);
     }
     else if(err == 0){
-        perror("connect()");
+        printf("connect success\n");
+        normalFlag = 1;
     }
 
-
-    //Send a message to server
-    char message[] = {"Give normal data."};
-    char recMessage[100] = {};
-    int s = send(sockfd, message, sizeof(message), 0);
-    if(s == -1){
+    char input1[256];
+    char input2[256];
+    char recMessage[100];;
+    if(send(sockfd, &portNum, sizeof(portNum), 0) == -1){
         perror("recv():");
     }
 
-    int flag = recv(sockfd, recMessage, sizeof(recMessage), 0);
-    if(flag == -1){
-        perror("recv():");
-    }
-    else if(flag == 0){
-        perror("recv():");
-    }
-    else{
-        printf("recv!\n");
-    }
+    memset(input1, 0, sizeof(input1));
+    memset(input2, 0, sizeof(input2));
+    memset(recMessage, 0, sizeof(recMessage));
 
-    printf("%s", recMessage);
-    printf("close Socket\n");
-    close(sockfd);
+    while(1){
+        read_set = master_set;
+        struct timeval time;
+        time.tv_sec = 1;
+        time.tv_usec = 0;
+
+        int s;
+        if((s = select(maxfd + 1, &read_set, NULL, NULL, &time)) <= 0){
+            if(s == 0){
+                if(normalFlag == 1){
+                    printf("input(normal) > ");
+                    fgets(input1, 256 , stdin);
+                }
+
+                if(priorityFlag == 1){
+                    printf("input(priority) > ");
+                    fgets(input2, 256 , stdin);
+                }
+
+                if(input1[strlen(input1) - 1] == '\n'){
+                    input1[strlen(input1) - 1] = '\0';
+                }
+                if(input2[strlen(input2) - 1] == '\n'){
+                    input2[strlen(input2) - 1] = '\0';
+                }
+
+                if(strcmp(input1, "exit") == 0){
+                    send(sockfd, input1, sizeof(input1), 0);
+                    close(sockfd);
+                    FD_CLR(sockfd, &master_set);
+                    normalFlag = 0;
+                }
+                else if(strlen(input1) >= 1 && input1[0] != '\0'){
+                    send(sockfd, input1, sizeof(input1), 0);
+                }
+
+                if(strcmp(input2, "exit") == 0){
+                    send(prifd, input2, sizeof(input2), 0);
+                    close(prifd);
+                    FD_CLR(prifd, &master_set);
+                    priorityFlag = 0;
+                }
+                else if(strlen(input2) >= 1 && input2[0] != '\0'){
+                    send(prifd, input2, sizeof(input2), 0);
+                }
+
+                // if two socket both close, leave the loop
+                if(normalFlag == 0 && priorityFlag ==0){
+                    break;
+                }
+
+                memset(input1, 0, sizeof(input1));
+                memset(input2, 0, sizeof(input2));
+            }
+            else{
+                perror("select()");
+                exit(0);
+            }
+        }
+        else{
+            for(int i = 0; i <= maxfd; i++){
+                if(FD_ISSET(i, &read_set)){
+                    memset(recMessage, 0, sizeof(recMessage));
+
+                    if(i == listenfd){
+                        struct sockaddr_in priAddr;
+
+                        socklen_t priLen = sizeof(priAddr);
+
+                        prifd = accept(listenfd, (struct sockaddr *)&priAddr, &priLen);
+                        if(prifd == -1){
+                            perror("accept()");
+                            exit(0);
+                        }
+                        else{
+                            printf("Accept priority socket \n");
+                            priorityFlag = 1;
+
+                            FD_SET(prifd, &master_set);
+                            if(prifd > maxfd){
+                                maxfd = prifd;
+                            }
+                        }
+                    }
+                    else{
+                        if(recv(i, recMessage, sizeof(recMessage), 0) <= 0){
+                            perror("recv()");
+                        }
+
+                        if(strlen(recMessage) > 0){
+                            printf("Server: %s\n", recMessage);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     return 0;
 }
--- server.c
+++ server.c
@@ -8,17 +8,41 @@
 #include <sys/time.h>
 #include <unistd.h>
 
+struct user{
+    int normalId;
+    int priorityId;
+    int port;
+    struct user* next;
+};
+
+struct user* rootUser; //to record userdata
+
+int connectToClient(int id, int port);
+
+void removeId(int id);
+
+fd_set activefd_set;
+int maxfd;
+
+int portNum = 8700; //port number is 8700
+char* address = "127.0.0.1"; //address is 127.0.0.1
+
 int main(int argc, char *argv[])
 {
-    fd_set activefd_set;
-    int maxfd;
+    // init rootUser
+    rootUser = (struct user *)malloc(sizeof(struct user));
+    rootUser->normalId = -1;
+    rootUser->priorityId = -1;
+    rootUser->port = -1;
+    rootUser->next = NULL;
 
     char buff[256];
     char message[] = {"Give priority data.\n"};
-    int sockfd = 0;
-    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+    int listenfd = 0;
+    //create socket
+    listenfd = socket(AF_INET , SOCK_STREAM , 0);
 
-    if (sockfd == -1){
+    if (listenfd == -1){
         perror("socket()");
     }
 
@@ -26,49 +50,48 @@ int main(int argc, char *argv[])
     bzero(&serverInfo,sizeof(serverInfo));
 
     serverInfo.sin_family = PF_INET;
-    serverInfo.sin_addr.s_addr = INADDR_ANY;
-    serverInfo.sin_port = htons(8700);
-    bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
-    listen(sockfd, 5);
+    serverInfo.sin_addr.s_addr = inet_addr(address);  //only for 127.0.0.1
+    serverInfo.sin_port = htons(portNum);  //only for port 8700
+
+    int option = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+
+    // bind it
+    bind(listenfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
+
+    // listen
+    listen(listenfd, 5);
 
     FD_ZERO(&activefd_set);
-    FD_SET(sockfd, &activefd_set);
-    maxfd = sockfd;
+    FD_SET(listenfd, &activefd_set);  //add listenfd to set
+    maxfd = listenfd;
 
     while(1){
-        int ret;
-        struct timeval tv;
         fd_set read_fds;
 
-        tv.tv_sec = 2;
-        tv.tv_usec = 0;
-
         read_fds = activefd_set;
-        ret = select(maxfd + 1, &read_fds, NULL, NULL, &tv);
-        if(ret == -1){
+        if(select(maxfd + 1, &read_fds, NULL, NULL, NULL) <= 0){
             perror("select()");
             exit(0);
         }
-        else if(ret == 0){
-            printf("timeout\n");
-        }
         else{
-            for(int i = 0; i < FD_SETSIZE; i++){
+            for(int i = 0; i <= maxfd; i++){
                 if(FD_ISSET(i, &read_fds)){
 
-                    if(i == sockfd){
+                    if(i == listenfd){
                         struct sockaddr_in clientAddr;
                         int clientfd;
 
                         socklen_t clientLen = sizeof(clientAddr);
 
-                        clientfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientLen);
+                        clientfd = accept(listenfd, (struct sockaddr *)&clientAddr, &clientLen);
                         if(clientfd == -1){
                             perror("accept()");
                             exit(0);
                         }
                         else{
                             printf("Accept client\n");
+                            printf("clientdf: %d\n", clientfd);
 
                             FD_SET(clientfd, &activefd_set);
                             if(clientfd > maxfd){
@@ -77,26 +100,151 @@ int main(int argc, char *argv[])
                         }
                     }
                     else{
-                        memset(buff, 0, sizeof(buff));
-                        int r = recv(i, buff, sizeof(buff), 0);
-                        if(r == -1){
-                            perror("recv()");
-                            exit(0);
+                        struct user* u = rootUser->next;
+                        int normalFlag = 0;
+                        int priorityFlag = 0;
+                        while(u != NULL){
+                            if(u->normalId == i || u->priorityId == i){
+                                if(u->normalId == i){normalFlag = 1;}
+                                else if(u->priorityId == i){priorityFlag = 1;}
+
+                                //printf("normalId: %d, priorityId: %d, listensd: %d\n", u->normalId, u->priorityId, listenfd);
+
+                                break;
+                            }
+                            u = u->next;
                         }
-                        else if(r == 0){
-                            perror("recv()");
+
+                        // process the data from client that has been connected
+                        if(normalFlag == 1 || priorityFlag == 1){
+                            //printf("normalFlag: %d, priorityFlag: %d\n", normalFlag, priorityFlag);
+                            memset(buff, 0, sizeof(buff));
+                            char sendData[256];
+                            memset(sendData, 0, sizeof(sendData));
+                            int rec;
+                            if((rec = recv(i, buff, sizeof(buff), 0)) <= 0){
+                                perror("recv() rec");
+                            }
+
+                            if(normalFlag == 1){
+                                memcpy(sendData, "[normal]You said: ", 18);
+                            }
+                            else if(priorityFlag == 1){
+                                memcpy(sendData, "[priotity]You said: ", 20);
+                            }
+
+                            if(strcmp(buff, "exit") == 0){
+                                close(i);
+                                FD_CLR(i, &activefd_set);
+                                removeId(i);
+                            }
+                            else{
+
+                                strcat(sendData, buff); //combine
+                                printf("output: %s\n", sendData);
+                                send(i, sendData, sizeof(sendData), 0);
+                            }
                         }
-                        else{
-                            printf("Receive: msg=[%s]\n", buff);
-                            send(i ,message,sizeof(message), MSG_OOB);  //send priority data
+                        // process the data from client that has not been connected
+                        else if(normalFlag == 0 && priorityFlag == 0){
+                            int port;
+                            if(recv(i, &port, sizeof(port), 0) <= 0){
+                                perror("recv() port");
+                            }
+                            else{
+                                printf("Receive: port = %d, user = %d\n", port, i);
+                                connectToClient(i, port);
+                            }
                         }
                     }
 
-                    close(i);
-                    FD_CLR(i, &activefd_set);
+                    /*close(i);
+                    FD_CLR(i, &activefd_set);*/
                 }
             }
         }
     }
     return 0;
+}
+
+int connectToClient(int id, int port){
+    struct user* newUser = (struct user *)malloc(sizeof(struct user));
+
+    int sockfd = socket(AF_INET , SOCK_STREAM , 0);
+
+    newUser->normalId = id;
+    newUser->port = port;
+    newUser->priorityId = sockfd;
+    newUser->next = NULL;
+
+    if (sockfd == -1){
+        perror("socket()");
+    }
+
+    struct sockaddr_in clientInfo;
+    bzero(&clientInfo, sizeof(clientInfo));
+    clientInfo.sin_family = PF_INET;
+
+    //localhost test
+    clientInfo.sin_addr.s_addr = inet_addr("127.0.0.1");
+    clientInfo.sin_port = htons(port);
+
+
+    int err = connect(sockfd,(struct sockaddr *)&clientInfo,sizeof(clientInfo));
+    if(err == -1){
+        perror("connect()");
+        exit(0);
+    }
+    else if(err == 0){
+        printf("connect success\n");
+    }
+
+    send(sockfd, "Hi, this is the priority socket!", 32, 0);
+
+    struct user* u = rootUser->next;
+    struct user* pre = rootUser;
+    while(u != NULL){
+        pre = u;
+        u = u->next;
+    }
+
+    pre->next = newUser;
+
+    FD_SET(sockfd, &activefd_set);
+    if(sockfd > maxfd){
+        maxfd = sockfd;
+    }
+
+    printf("new connection, normal socket: %d, priority socket: %d, client port: %d, priority port: %d\n", newUser->normalId, newUser->priorityId, portNum, newUser->port);
+
+    /*struct user* temp = rootUser->next;
+    while(temp != NULL){
+        printf("normalId: %d, priorityId: %d, port: %d\n", temp->normalId, temp->priorityId, port);
+        temp = temp->next;
+    }*/
+}
+
+void removeId(int id){
+    struct user* u = rootUser->next;
+    struct user* pre = rootUser;
+
+    while(u != NULL){
+        if(u->normalId == id){
+            u->normalId = -1;
+            printf("normalId: %d leave\n", id);
+        }
+        if(u->priorityId == id){
+            u->priorityId = -1;
+            printf("priorityId: %d leave\n", id);
+        }
+
+        if(u->normalId == -1 && u->priorityId == -1){
+            pre->next = u->next;
+            printf("A user out\n");
+            free(u);
+        }
+
+        pre = u;
+        u = u->next;
+    }
 }
